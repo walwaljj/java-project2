6a4673b32c651346149f16b052694b566a9a29ee
Shjung <wjdtngus9@gmail.com>
2023-04-19 13:09:25 +0900

    feat : LocalDateTime 클래스 생성 - LocalDateTime 클래스 생성

---
+++ src/main/java/com/example/javaproject2/LocalDateTime.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2;
+
+public class LocalDateTime {
+    private LocalDateTime localDateTime;
+
+    public LocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+}
