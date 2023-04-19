4fc129ed43904f43960b0b9ae30817d5589f03b6
Shjung <wjdtngus9@gmail.com>
2023-04-19 14:47:29 +0900

    feat : BufferedReaderEx, BufferedReaderExTest 클래스 생성 - bufferReader로 한줄씩 읽어보기

---
+++ src/main/java/com/example/javaproject2/week1/BufferedReaderEx.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderEx {
+    public void readALine() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(is);
+
+        System.out.println("buffered1.readLine() = "+buffer.readLine());
+        System.out.println("buffered2.readLine() "+buffer.readLine());
+
+
+    }
+}
