36006329539c6b951a68f102bb4a92caa4bc53b4
Shjung <wjdtngus9@gmail.com>
2023-04-19 14:20:24 +0900

    feat : JavaInput 추가 - inputStream테스트

---
+++ src/main/java/com/example/javaproject2/week1/JavaInputTest.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week1;
+
+import java.io.IOException;
+
+public class JavaInputTest {
+    public static void main(String[] args) throws IOException {
+        JavaInput javaInput = new JavaInput();
+        javaInput.readTwoChars(); // javaInput 은 throws 를 사용해 사용하는 클래스에 에러를 던짐.
+                                //이 에러를 여기서 해결 하거나 , 사용시 마다 해결해야함.
+
+        // .read 는 아스키 코드를 읽어옴.
+        //123을 입력하면 49 가 나옴. 1만 읽음
+        //abc를 입력하면 97 이 나옴. a만 읽음.
+
+    }
+}
