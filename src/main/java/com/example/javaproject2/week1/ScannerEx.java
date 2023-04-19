4b4eae41816e98c9a3a66856af4a14728c42f4ce
Shjung <wjdtngus9@gmail.com>
2023-04-19 15:29:15 +0900

    feat : ScannerEx, ScannerExTest 클래스 생성 - scanner 기본 사용 코드 추가

---
+++ src/main/java/com/example/javaproject2/week1/ScannerEx.java
@@ -0,0 +1,19 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public void plusTowNumbers(int val1, int val2) {
+
+    }
+
+    public void readTwoNumbersAndPlus(){
+
+        //숫자 2개 입력받기
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+
+
+}
---
+++ src/main/java/com/example/javaproject2/week1/ScannerExTest.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumbersAndPlus();
+
+    }
+}
