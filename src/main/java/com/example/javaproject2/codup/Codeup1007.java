366dfbe409091fdf27e3479b88da9cae72b64b95
Shjung <wjdtngus9@gmail.com>
2023-04-20 10:02:13 +0900

    feat : codeup 1007 ~ 8 코드 - 1007  / 1008 문제 풀이

---
+++ src/main/java/com/example/javaproject2/codup/Codeup1007.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.codup;
+
+public class Codeup1007 {
+    public static void main(String[] args) {
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codup/Codeup1008.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codup;
+
+public class Codeup1008 {
+    /**
+     * ┌┬┐
+     * ├┼┤
+     * └┴┘
+     */
+    public static void main(String[] args) {
+        System.out.println("\u250C\u252C\u2510\n\u251c\u253c\u2524\n\u2514\u2534\u2518");
+
+    }
+}
