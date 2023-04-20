029c717fc9f3d75672db75956ea19bb5f2e79ec2
Shjung <wjdtngus9@gmail.com>
2023-04-20 11:33:15 +0900

    feat : codeup 1010 코드 - 1010 문제풀이

---
+++ src/main/java/com/example/javaproject2/codup/Codeup1010.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codup;
+
+import java.util.Scanner;
+
+public class Codeup1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+
+    }
+}
