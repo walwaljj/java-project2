251889abd9d3b84a389be5f8f7bc0c22556edf5e
Shjung <wjdtngus9@gmail.com>
2023-04-20 15:17:28 +0900

    feat :  변수선언과 초기화 - VariableEx , ByteTypeEx 클래스 추가

---
+++ src/main/java/com/example/javaproject2/week1/day4/ByteTypeEx.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.week1.day4;
+
+public class ByteTypeEx {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = -127;
+        byte byte3 = -128;
+//        byte byte4 = 128; // byte 는 -128 ~ 127 까지 담을 수 있고 이외는 에러.
+//        byte byte5 = -129;
+        System.out.println("byte1 = " + byte1);
+        System.out.println("byte2 = " + byte2);
+        System.out.println("byte3 = " + byte3);
+
+
+
+    }
+}
