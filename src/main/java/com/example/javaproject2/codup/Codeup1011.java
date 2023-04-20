6e66fe0aaf311bd37e1d1b708eda8fddb746a40c
Shjung <wjdtngus9@gmail.com>
2023-04-20 13:44:13 +0900

    feat :  codeup 1011 - 1013 코드 - 1011 / 1012 / 1013 문제 풀이

---
+++ src/main/java/com/example/javaproject2/codup/Codeup1011.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codup;
+
+import java.util.Scanner;
+
+public class Codeup1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char x = sc.next().charAt(0);
+        System.out.printf("%s",x);
+
+    }
+}
