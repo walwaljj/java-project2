92e760334f89cd8c9cd98b115ef014b52439d956
Shjung <wjdtngus9@gmail.com>
2023-04-20 09:27:11 +0900

    feat : ScannerRemindTest, ScannerRemind 클래스 생성 - scanner 로 숫자 더하기

---
+++ src/main/java/com/example/javaproject2/week1/ScannerRemind.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerRemind {
+
+
+    public void readANum() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/ScannerRemindTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1;
+
+public class ScannerRemindTest {
+    public static void main(String[] args) {
+        ScannerRemind sc = new ScannerRemind();
+        sc.readANum();
+    }
+}
