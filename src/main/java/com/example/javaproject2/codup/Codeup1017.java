6c69d1cc7232d0dd2f950581c4e419a0b49eb6cc
Shjung <wjdtngus9@gmail.com>
2023-04-21 10:25:04 +0900

    feat : codeup 1017 문제 - 1017 문제 풀이

---
+++ src/main/java/com/example/javaproject2/codup/Codeup1017.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codup;
+
+import java.util.Scanner;
+
+public class Codeup1017 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.printf("%d %d %d",num,num,num);
+
+    }
+}
