b188cc0681b7f668922ba67a415fcb8368a5f031
Shjung <wjdtngus9@gmail.com>
2023-04-27 13:25:48 +0900

    feat : 2023-04-27 codeup ~1058

--- src/main/java/com/example/javaproject2/codup/Codeup1058.java
+++ src/main/java/com/example/javaproject2/codup/Codeup1058.java
@@ -8,7 +8,7 @@ public class Codeup1058 {
         String[] strNum = sc.nextLine().split(" ");
         int num1 = Integer.parseInt(strNum[0]);
         int num2 = Integer.parseInt(strNum[1]);
-        int isTrue = num1 == 1 && num2 == 1  ? 1 : 0;
+        int isTrue = num1 == 0 && num2 == 0  ? 1 : 0;
         System.out.println(isTrue);
     }
 }
