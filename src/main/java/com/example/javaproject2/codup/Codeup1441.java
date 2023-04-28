89f362d561e2f1a17852e9ca8249aecfe73604fe
Shjung <wjdtngus9@gmail.com>
2023-04-28 13:44:03 +0900

    feat : 2023-04-28 codeup1441, SwitchCaseClinicHours 추가

--- src/main/java/com/example/javaproject2/codup/Codeup1441.java
+++ src/main/java/com/example/javaproject2/codup/Codeup1441.java
@@ -1,11 +1,22 @@
 package com.example.javaproject2.codup;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Codeup1441 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int howManyNum = sc.nextInt();
+        int[] numArr = new int[howManyNum];
+        for(int i = 0 ; i < numArr.length; i++){
+            numArr[i] = sc.nextInt();
+        }
 
+        int temp = 0;
+        Arrays.sort(numArr);
+        for (int num : numArr) {
+            System.out.println(num);
+        }
     }
 }
