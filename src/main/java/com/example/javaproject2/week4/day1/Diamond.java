d0801e5296ab5fb6d95480240464d76605539ebd
Shjung <wjdtngus9@gmail.com>
2023-05-08 10:49:43 +0900

    다이아몬드 별 찍기

---
+++ src/main/java/com/example/javaproject2/week4/day1/Diamond.java
@@ -0,0 +1,37 @@
+package com.example.javaproject2.week4.day1;
+
+import java.util.Scanner;
+
+public class Diamond {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int starCount = 1;
+        int half = num / 2;
+        int spaceCount = (num - starCount ) / 2 ;
+        for (int i = 0; i < num; i++) {
+
+            for (int j = 0; j < spaceCount; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < starCount; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < spaceCount; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+
+            if( i >= half ){
+                starCount -= 2;
+                spaceCount += 1;
+            }else {
+                starCount += 2;
+                spaceCount -= 1;
+            }
+        }
+
+
+    }
+}
