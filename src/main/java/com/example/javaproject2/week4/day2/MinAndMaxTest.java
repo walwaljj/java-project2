4da368119dd74c0bdb5f8e0cbe405fae6df9e5d5
Shjung <wjdtngus9@gmail.com>
2023-05-09 17:07:03 +0900

    feat : Min, Max 추상화

---
+++ src/main/java/com/example/javaproject2/week4/day2/MinAndMaxTest.java
@@ -0,0 +1,37 @@
+package com.example.javaproject2.week4.day2;
+
+abstract class IsMinOrMax {
+   public abstract boolean isMinOrMax(int num1, int num2);
+   public int result(int[] arr){
+      int result = arr[0];
+      for (int i = 0; i < arr.length; i++) {
+         if(isMinOrMax(result , arr[i])) result = arr[i];
+      }
+      return result;
+   }
+}
+class MaxNum extends IsMinOrMax{
+   @Override
+   public boolean isMinOrMax(int maxNum, int num) {
+      return maxNum < num ? true : false;
+   }
+}
+
+class MinNum extends IsMinOrMax{
+   @Override
+   public boolean isMinOrMax(int minNum, int num) {
+      return minNum > num ? true : false;
+   }
+}
+public class MinAndMaxTest{
+   public static void main(String[] args) {
+      IsMinOrMax max = new MaxNum();
+      IsMinOrMax min = new MinNum();
+
+      int[] arr = {5, 6, 8, 1, 3};
+
+      System.out.printf("%d %d",max.result(arr),min.result(arr));
+
+   }
+}
+
