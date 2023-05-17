755246af087a9ba07412033e07dd4c4bf3ff6477
Shjung <wjdtngus9@gmail.com>
2023-05-17 09:01:55 +0900

    feat : BubbleSort

--- src/main/java/com/example/javaproject2/week5/day1/BubbleSort01.java
+++ src/main/java/com/example/javaproject2/week5/day1/BubbleSort01.java
@@ -40,14 +40,14 @@ public class BubbleSort01 {
 //                }
 //            }
 //        }
-//        for (int i = 1; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             for (int j = 0; j < arr.length-1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp2 = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp2;
                 }
-//            }
+            }
         }
         System.out.println(Arrays.toString(arr));
     }
