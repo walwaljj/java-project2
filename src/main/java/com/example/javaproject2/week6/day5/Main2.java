1743dd11d98ad9db6e3fddc63779cd3de2e966bb
Shjung <wjdtngus9@gmail.com>
2023-05-26 16:16:13 +0900

    feat : Shape interface, Circle class calculatePerimeter 메서드 추가 , Rectangle class 추가

--- src/main/java/com/example/javaproject2/week6/day5/Main2.java
+++ src/main/java/com/example/javaproject2/week6/day5/Main2.java
@@ -6,8 +6,13 @@ interface Shape {
     default void display(){
         System.out.println("디폴트 메서드"); // 구현하지 않아도 사용가능하다.
     }
+
+    double calculatePerimeter();
 }
 
+
+
+// TODO: Circle 클래스 작성
 class Circle implements Shape{
     private double radius;
 
@@ -20,13 +25,55 @@ class Circle implements Shape{
 //        return radius * radius * 3.14 ;
         return Math.PI * radius * radius;
     }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+
+// TODO: Rectangle 클래스 작성
+
+class Rectangle implements Shape{
+    private int width;
+    private int length;
+
+    public Rectangle(int width, int length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * length;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return (width + length) * 2;
+    }
 }
 
 public class Main2{
     public static void main(String[] args){
-        Circle circle = new Circle(3);
-        System.out.println(circle.calculateArea());
-        circle.display();
+//        Circle circle = new Circle(3);
+//        System.out.println(circle.calculateArea());
+//        circle.display();
+
+        Shape rectangle = new Rectangle(5, 10);
+        Shape circle = new Circle(3.5);
+
+        double area1 = rectangle.calculateArea();
+        double perimeter1 = rectangle.calculatePerimeter();
+
+        double area2 = circle.calculateArea();
+        double perimeter2 = circle.calculatePerimeter();
+
+        System.out.println("사각형의 넓이: " + area1);
+        System.out.println("사각형의 둘레: " + perimeter1);
+        System.out.println("원의 넓이: " + area2);
+        System.out.println("원의 둘레: " + perimeter2);
 
     }
 }
