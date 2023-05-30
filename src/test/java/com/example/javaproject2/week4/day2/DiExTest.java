b449bf2d4c8e90137d615d70ade46843a7706756
Shjung <wjdtngus9@gmail.com>
2023-05-30 11:33:01 +0900

    feat : DiExTest refactoring

--- src/test/java/com/example/javaproject2/week4/day2/DiExTest.java
+++ src/test/java/com/example/javaproject2/week4/day2/DiExTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DiExTest {
 
     public static void main(String[] args) {
-        DiEx di1 = new DiEx(new ParallelogramShapeDrawer());
+        DiEx1 di1 = new DiEx1(new ParallelogramShapeDrawer());
         di1.doSth2();
     }
 
