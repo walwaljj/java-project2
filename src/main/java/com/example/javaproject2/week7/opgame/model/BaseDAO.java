2eac1c3a1583ee4578e03cbb04a6caaa40796a5d
Shjung <wjdtngus9@gmail.com>
2023-06-01 11:44:40 +0900

    feat : BaseDAO 수정

---
+++ src/main/java/com/example/javaproject2/week7/opgame/model/BaseDAO.java
@@ -0,0 +1,42 @@
+
+package com.example.javaproject2.week7.opgame.model;
+import java.sql.*;
+
+public class BaseDAO {
+    protected Connection conn = null;
+    protected Statement smt = null;
+    protected PreparedStatement psmt = null;
+    protected ResultSet rs = null;
+
+    protected void getConn(){
+        String url = "jdbc:sqlite:world.db";
+        try {
+            conn = DriverManager.getConnection(url);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    protected void close(){
+        try {
+            if (rs != null) rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            if (smt != null) smt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            if (psmt != null) psmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
