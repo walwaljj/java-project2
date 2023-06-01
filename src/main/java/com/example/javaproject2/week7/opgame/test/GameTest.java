8994ee29e7115fe6af1a9c1902f3f41667824a53
Shjung <wjdtngus9@gmail.com>
2023-06-01 11:44:23 +0900

    feat : GameTest

--- src/main/java/com/example/javaproject2/week7/opgame/test/GameTest.java
+++ src/main/java/com/example/javaproject2/week7/opgame/test/GameTest.java
@@ -1,5 +1,6 @@
 package com.example.javaproject2.week7.opgame.test;
 import com.example.javaproject2.week7.opgame.service.OpGame;
+import com.example.javaproject2.week7.opgame.service.PlusOperatorLevelOne;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,29 +8,48 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GameTest {
 
-    @Test
-    public void test(){
-        OpGame  game = new OpGame();
+    public void test() {
+        OpGame game = new OpGame(new PlusOperatorLevelOne());
+
         game.makeQuestion();
         String question = game.getQuestion();
-        assertEquals("1 + 1 = ",question);
-        assertEquals(3,game.getRemainingAnswers());
-        assertEquals(false,game.isAnswer(3));
-        assertEquals(2,game.getRemainingAnswers());
-        assertEquals("한번 더 해보자",game.getCheeringMsg() );
+
+        assertEquals("1 + 1 = ", question);
+        assertEquals(3, game.getRemainingAnswers());
+        assertEquals(false, game.isAnswer(3));
+        assertEquals(2, game.getRemainingAnswers());
+        assertEquals("한번 더 해보자", game.getCheeringUpMsg());
         assertEquals(true, game.isAnswer(2));
     }
 
     @Test
-    public void testSumMax(){
-        int a = 0;
-        int b = 0;
-        assertTrue(a + b < 10);
+    public void testRandom() {
+        System.out.println( (int)(Math.random() * 10) );
     }
 
     @Test
-    public void testRandom(){
-        System.out.println((int)Math.random() * 10);
+    public void testSumMax() {
+        int max = 10;
+        int a = (int)(Math.random() * max);
+        int b = (int)(Math.random() * (max - a));
+        System.out.printf("%d + %d = %d", a, b, a+b);
+        assertTrue(a + b < max);
     }
-
+    @Test
+    public void testMinus() {
+        int max = 10;
+//        int a = (int)(Math.random() * max);
+//        int b = (int)(Math.random() * a);
+        int a = (int)(Math.random() * max);
+        int b = (int)(Math.random() * max);
+        if (a < b) {
+            int tmp = b;
+            a = b;
+            b = tmp;
+        }
+        System.out.printf("%d - %d = %d", a, b, a-b);
+        assertTrue(a - b >= 0);
+    }
 }
+
+
