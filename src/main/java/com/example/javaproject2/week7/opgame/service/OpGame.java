e11b314e3d9bdd4bad205758e537b46a6e09167f
Shjung <wjdtngus9@gmail.com>
2023-06-01 11:42:21 +0900

    feat : 두 수를 더해서 9이하 문제출제, 여러 게임레벨 지원준비

--- src/main/java/com/example/javaproject2/week7/opgame/service/OpGame.java
+++ src/main/java/com/example/javaproject2/week7/opgame/service/OpGame.java
@@ -2,15 +2,25 @@ package com.example.javaproject2.week7.opgame.service;
 
 public class OpGame {
     int remainingAnswers;
-    int a, b;
+    String cheeringUpMsg = "한번 더 해보자";
+    IOperator op;
+
+    public OpGame(IOperator op) {
+        this.op = op;
+    }
+
     public void makeQuestion() {
         remainingAnswers = 3;
-        a = 1;
-        b = 1;
+        op.setA(1);
+        op.setB(1);
+    }
+
+    public void makeQuestion(int max) {
+        op.generateQuestion(max);
     }
 
     public String getQuestion() {
-        return String.format("%d + %d = ",a,b);
+        return op.getQuestionMsg();
     }
 
     public int getRemainingAnswers() {
@@ -18,11 +28,12 @@ public class OpGame {
     }
 
     public boolean isAnswer(int answer) {
-        remainingAnswers --;
-        return (a + b) == answer;
+        remainingAnswers--;
+        return op.isEqueals(answer);
     }
 
-    public String getCheeringMsg() {
-        return "한번 더 해보자";
+    public String getCheeringUpMsg() {
+        return cheeringUpMsg;
     }
+
 }
